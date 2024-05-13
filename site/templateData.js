0ac91378f97b36db54a2590a72483e115f621e67
astro <astronomy.dev@gmail.com>
2024-05-13 15:35:40 +0300

    weekly list now works with js file, added template for times

--- site/templateData.js
+++ site/templateData.js
@@ -2,61 +2,73 @@ const categoryTemplate = document.querySelector("#category-template");
 const entryTemplate = document.querySelector("#entry-template");
 
 const dailiesNode = document.querySelector("#dailies");
+const weekliesNode = document.querySelector("#weeklies");
 
 const dailies = [
     {
         title: "Arcane River Dailies",
-        subDailies: ["Vanishing Journey", "Chu-Chu Island", "Lachelein", "Arcana", "Morass", "Esfera", "Tenebris"]
+        subContent: ["Vanishing Journey", "Chu-Chu Island", "Lachelein", "Arcana", "Morass", "Esfera", "Tenebris"],
     },
 
     {
         title: "Grandis Dailies",
-        subDailies: ["Cernium", "Arcus", "Odium", "Shangri-La", "Arteria", "Carcion", "Erda's Request"]
+        subContent: ["Cernium", "Arcus", "Odium", "Shangri-La", "Arteria", "Carcion", "Erda's Request"],
     },
 
     {
         title: "Bosses",
-        subDailies: ["Zakum", "Magnus", "Hilla", "Papulatus", "Pierre", "Von Bon", "Crimson Queen", "Vellum", "Von Leon", "Horntail", "Arkarium", "Pink Bean", "Gollux"]
+        subContent: ["Zakum", "Magnus", "Hilla", "Papulatus", "Pierre", "Von Bon", "Crimson Queen", "Vellum", "Von Leon", "Horntail", "Arkarium", "Pink Bean", "Gollux"],
     },
 
     {
         title: "Misc.",
-        subDailies: ["Monster Park", "Maple Tour", "Ursus", "Legion", "Commerci", "Event Coin Cap", "Profession Stamina", "Threads of Fate", "Monster Collection", "Maple Home", "Fairy Bros Daily Gift", "Fairy Bros Golden Giveaway"]
-    }
+        subContent: ["Monster Park", "Maple Tour", "Ursus", "Legion", "Commerci", "Event Coin Cap", "Profession Stamina", "Threads of Fate", "Monster Collection", "Maple Home", "Fairy Bros Daily Gift", "Fairy Bros Golden Giveaway"],
+    },
         
 ];
 
 const weeklies = [
     {
         title: "Arcane River Weeklies",
-        subWeeklies: ["Vanishing Journey", "Chu-Chu Island", "Lachelein", "Arcana", "Morass", "Esfera"]
+        subContent: ["Vanishing Journey", "Chu-Chu Island", "Lachelein", "Arcana", "Morass", "Esfera"],
     },
 
     {
         title: "Bosses",
-        subWeeklies: ["C. Zakum", "HMagnus", "HHilla", "HPapulatus", "C. Pierre", "C. Von Bon", "C. Crimson Queen", "C. Pink Bean", "Cygnus", "Lotus", "Damien", "Guardian Angel Slime", "Lucid", "Will", "Gloom", "VHilla", "PNo", "Akechi"]
+        subContent: ["C. Zakum", "HMagnus", "HHilla", "HPapulatus", "C. Pierre", "C. Von Bon", "C. Crimson Queen", "C. Pink Bean", "Cygnus", "Lotus", "Damien", "Guardian Angel Slime", "Lucid", "Will", "Gloom", "VHilla", "PNo", "Akechi"],
     },
 
     {
         title: "Misc.",
-        subWeeklies: ["Scrapyard Weeklies", "Dark World Tree Weeklies", "Culvert", "Event Weeklies"]
+        subContent: ["Scrapyard Weeklies", "Dark World Tree Weeklies", "Culvert", "Event Weeklies"],
+    },
+];
+
+function addContent(content, idNode) {
+    for (attribute of content) {
+        const categoryNode = categoryTemplate.content.cloneNode(true);
+        const titleNode = categoryNode.querySelector(".collapse-title");
+        const formControlNode = categoryNode.querySelector(".form-control");
+
+        titleNode.textContent = attribute.title
+        for (const entry of attribute.subContent) {
+            const entryNode = entryTemplate.content.cloneNode(true);
+            const spanNode = entryNode.querySelector("span");
+            spanNode.textContent = entry;
+    
+            formControlNode.appendChild(entryNode);
+        }
+
+        idNode.appendChild(categoryNode);
     }
-]
-
-for (daily of dailies) {
-    const categoryNode = categoryTemplate.content.cloneNode(true);
-    const titleNode = categoryNode.querySelector(".collapse-title");
-    const formControlNode = categoryNode.querySelector(".form-control");
-
-    titleNode.textContent = daily.title
-    for (const subDaily of daily.subDailies) {
-        const entryNode = entryTemplate.content.cloneNode(true);
-        const spanNode = entryNode.querySelector("span");
-        spanNode.textContent = subDaily;
-
-        formControlNode.appendChild(entryNode);
-    }
-
-    dailiesNode.appendChild(categoryNode);
 }
 
+addContent(dailies, dailiesNode);
+addContent(weeklies, weekliesNode);
+
+const currentTime = new Date();
+const resetTime = new Date(0, 0, 0, 0, 0)
+const timeUntilReset = resetTime.getSeconds() - currentTime.getSeconds()
+if (timeUntilReset <= 0) {
+    
+}
